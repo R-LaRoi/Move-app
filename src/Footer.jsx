55821fa7cafeb9fff90025ae394a85e0b0141ca5
R. LaRoi <are@raewhoo.com>
2023-06-14 19:25:28 +0200

    +Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+export default function Footer(){
+
+  return(
+
+
+    <section>
+
+      <ul>
+        <li>spotify</li>
+        <li>art</li>
+        <li>linkedIn</li>
+
+      </ul>
+
+    </section>
+  )
+
+
+}
+
--- src/MeetRachel.jsx
+++ src/MeetRachel.jsx
@@ -15,16 +15,24 @@ return (
 <img src={pic}  className="meet-pic"alt="woman excercising outdoors"
  />
 
-  <p>
+  <div className='meet-text'>
     <h1>meet rachel</h1>
 always on. high energy unmatched. <br></br>
             <br></br>here to motivate you through every minute of your fitness
             journey. focusing on body weight movements to help you strengthen,
             condition and improve balance. rachel is ready to partner with you
             and push beyond your expectations. wherever you are. whenever you're
-            ready. <br></br>let's move!{" "}</p>
+            ready. <br></br>let's move!{" "}</div>
+
+          
 </div>
+
    
+ <div className='spotify'>
+
+<iframe src="https://open.spotify.com/embed/playlist/02LtXdK3vRDKtKSiQxuTIq?utm_source=generator" width="100%" height="375" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+</div> 
 
             
 </section>

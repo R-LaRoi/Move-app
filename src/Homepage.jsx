92f50f4d2ee0a594ed2602ffface3ada0d6ed1cf
R. LaRoi <are@raewhoo.com>
2023-06-15 15:08:35 +0200

    Updated Video

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -14,24 +14,24 @@ export default function Homepage() {
       <video autoPlay loop muted id="mobile-vid">
         <source 
           src="
-https://github.com/R-LaRoi/Move-app/assets/114012059/a6428fa5-0f4d-41f0-8955-be3153861000"
+https://github.com/R-LaRoi/Move-app/assets/114012059/d13eb87f-9cb6-49ad-a563-732767ae3775"
           type="video/mp4"
         />
       </video>
 
-<div className='move-video'>
+{/* <div className='move-video'>
        <video autoPlay loop muted id="dsk-vid">
         <source
           src="
-https://user-images.githubusercontent.com/114012059/218713471-633594a8-adc7-4600-90c1-95f4a1347ff6.mp4"
+https://github.com/R-LaRoi/Move-app/assets/114012059/d13eb87f-9cb6-49ad-a563-732767ae3775"
           type="video/mp4"
         />
       </video>
-</div>
-      <div className='header'> <span>consistency is key.</span>
+</div> */}
+      <div className='header'> <span>what's the move</span>
       <div className='h-btn'>
      <Link to = "/meetrachel"> <button className='home-btn'>meet rachel</button></Link>
-         <Link to = "/dailyroutine">  <button className='home-btn'>what's the move</button></Link>
+         <Link to = "/dailyroutine">  <button className='home-btn'>let's move</button></Link>
          </div></div>
       
 </section>

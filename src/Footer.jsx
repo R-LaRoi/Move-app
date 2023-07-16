6a16dbb0f030873ebb16298b06a1e9812bb0f4ac
R. LaRoi <are@raewhoo.com>
2023-07-16 08:48:59 +0200

    updated footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,10 +10,22 @@ export default function Footer(){
 
       <ul>
     
-        <li><i className="fa-solid fa-link"></i></li>
-        <li><i className="fa-brands fa-linkedin"></i></li>
-        <li><i className="fa-brands fa-github"></i></li>
-   <li><i className="fa-brands fa-spotify"></i></li>
+        <li>
+          <a href="https://www.rachelstroy.com/" target='_blank' >
+          <i className="fa-solid fa-link" ></i></a></li>
+        <li>
+          <a href="https://www.linkedin.com/in/rachel-stroy-978397277/" target='_blank'>
+          <i className="fa-brands fa-linkedin"></i></a></li>
+        <li>
+          <a href="https://github.com/R-LaRoi" target='_blank'>
+          <i className="fa-brands fa-github"></i></a></li>
+   <li>
+     <a href= "https://open.spotify.com/playlist/02LtXdK3vRDKtKSiQxuTIq?si=0ce81940fb9d48a8">  <i className="fa-brands fa-spotify" target='_blank'>
+   </i> </a>
+    
+  
+   
+   </li>
     <div className='ft'>Disclaimer 2023</div>
       </ul>
 
